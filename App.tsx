76e8a9fbaeb220a84ec1cac867adde3046996eda
JosiasAurel <ndjosiasaurel@gmail.com>
2023-09-15 12:15:00 +0100

    create basic product card

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { GluestackUIProvider, Text, Box, config, Button, ToastDescription, ToastTitle } from "@gluestack-ui/themed"
 import { Toast, useToast, VStack, ButtonText } from "@gluestack-ui/themed";
+import Product from "./components/Product";
 
 function Example() {
   const toast = useToast()
@@ -32,9 +33,11 @@ function Example() {
 export default function App() {
   return (
     <GluestackUIProvider config={config.theme}>
-      <Box width="100%" justifyContent="center" alignItems="center">
+      <Box width="100%" height="100%" justifyContent="center" alignItems="center">
+        <Text>Just another piece of text</Text>
         <Text>Open up App.js to start working on your app!</Text>
        <Example /> 
+       <Product name="Sample" price={1000} description="Please buy this beautiful landscape picture I'm begging I'm poor." />
       </Box>
     </GluestackUIProvider>
   )
---
+++ components/Product.tsx
@@ -0,0 +1,74 @@
+import React, { useRef, useEffect } from "react";
+import { StyleSheet } from "react-native";
+import { Box, Image, Text, HStack, styled, Center, Icon, StarIcon, VStack, Button, Divider } from "@gluestack-ui/themed";
+import { Cart, Star } from "iconoir-react-native";
+
+type ProductProps = {
+    name: string,
+    price: number,
+    description: string // a short description of the product
+}
+
+const styles = StyleSheet.create({
+    image: {
+        backgroundColor: "$yellow100",
+        padding: "auto",
+    }
+});
+
+const StyledImage = styled(Image, styles.image);
+
+const Product: React.FC<ProductProps> = ({ name, price, description }) => {
+    // const boxRef = useRef();
+    // useEffect(() => {
+    //     const styleObj = {
+    //         borderColor: "red",
+    //         borderWidth: 2,
+    //         borderRadius: "8"
+    //     };
+    //     boxRef.current.setNativeProps({ style: styleObj });
+    // }, [boxRef]);
+    return (
+        <Box p="$3" margin="$2" w="$48" borderWidth="$2" borderColor="$blueGray300" borderRadius="$xl">
+            <Center>
+                <StyledImage
+                    w={180}
+                    h={180}
+                    borderRadius="$md"
+                    source={{
+                        uri: "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+                    }}
+                />
+            </Center>
+
+            <VStack marginTop="$2" width="$full" >
+                <HStack space="$4xl" w="$full" alignItems="center">
+                    <Text color="black" bold w="$32" size="lg">
+                        {name}
+                    </Text>
+
+                    <Button bgColor="transparent" width={36}>
+
+                        <Star color={"black"} width={24} height={24} />
+                    </Button>
+                </HStack>
+
+                <Text marginTop="$1" maxHeight={80} w="$full" size="sm">
+                    {description.length > 45 ? description.slice(0, 45) + "..." : description}
+                </Text>
+
+                <HStack marginTop="$2" w="$full" space="4xl" alignItems="center">
+                    <Text color="black" bold size="xl">
+                        {price.toLocaleString()} XAF
+                    </Text>
+                    <Button width={34} height={35} bgColor="#ffda00">
+                        <Cart color={"white"} height={20} width={20} />
+                    </Button>
+                </HStack>
+
+            </VStack>
+        </Box>
+    )
+}
+
+export default Product;
