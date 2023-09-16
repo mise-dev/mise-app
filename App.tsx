2f3ce5c4436fceab357de2412c536669852b6410
JosiasAurel <ndjosiasaurel@gmail.com>
2023-09-16 16:45:48 +0100

    add views

--- App.tsx
+++ App.tsx
@@ -1,44 +1,29 @@
-import { GluestackUIProvider, Text, Box, config, Button, ToastDescription, ToastTitle } from "@gluestack-ui/themed"
-import { Toast, useToast, VStack, ButtonText } from "@gluestack-ui/themed";
-import Product from "./components/Product";
+import { GluestackUIProvider,config } from "@gluestack-ui/themed"
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-function Example() {
-  const toast = useToast()
-  return (
-    <Button
-      onPress={() => {
-        toast.show({
-          placement: "top",
-          render: ({ id }) => {
-            return (
-              <Toast nativeId={id} action="attention" variant="solid">
-                <VStack space="xs">
-                  <ToastTitle>New Message</ToastTitle>
-                  <ToastDescription>
-                    Hey, just wanted to touch base and see how you're doing.
-                    Let's catch up soon!
-                  </ToastDescription>
-                </VStack>
-              </Toast>
-            )
-          },
-        })
-      }}
-    >
-      <ButtonText>Press Me</ButtonText>
-    </Button>
-  )
-}
+// import screens here
+import Home from "./Views/Home";
+import KitchenSink from "./Views/KitchenSink";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <GluestackUIProvider config={config.theme}>
-      <Box width="100%" height="100%" justifyContent="center" alignItems="center">
-        <Text>Just another piece of text</Text>
-        <Text>Open up App.js to start working on your app!</Text>
-       <Example /> 
-       <Product name="Sample" price={1000} description="Please buy this beautiful landscape picture I'm begging I'm poor." />
-      </Box>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* screens */}
+          <Stack.Screen
+            name="Home"
+            component={Home}
+          />
+          <Stack.Screen name="Kitchen" component={KitchenSink} />
+
+          {/* end screens */}
+        </Stack.Navigator>
+      </NavigationContainer>
     </GluestackUIProvider>
   )
 }
+
---
+++ Views/Home.tsx
@@ -0,0 +1,48 @@
+import { Toast, useToast, VStack, ButtonText, Button, ToastDescription, Text, Box, ToastTitle } from "@gluestack-ui/themed";
+import Product from "../components/Product";
+
+export default function Home({ navigation }) {
+    return (
+
+        <Box width="100%" height="100%" justifyContent="center" alignItems="center">
+            <Text>Just another piece of text</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Example />
+            <Button onPress={() => navigation.navigate("Kitchen")}>
+                <ButtonText>
+                    Kitchen Sink
+                </ButtonText>
+            </Button>
+            <Product name="Sample" price={1000} description="Please buy this beautiful landscape picture I'm begging I'm poor." />
+        </Box>
+    )
+}
+
+function Example() {
+    const toast = useToast()
+    return (
+        <Button
+            onPress={() => {
+                toast.show({
+                    placement: "top",
+                    render: ({ id }) => {
+                        return (
+                            <Toast nativeId={id} action="attention" variant="solid">
+                                <VStack space="xs">
+                                    <ToastTitle>New Message</ToastTitle>
+                                    <ToastDescription>
+                                        Hey, just wanted to touch base and see how you're doing.
+                                        Let's catch up soon!
+                                    </ToastDescription>
+                                </VStack>
+                            </Toast>
+                        )
+                    },
+                })
+            }}
+        >
+            <ButtonText>Press Me</ButtonText>
+        </Button>
+    )
+}
+
