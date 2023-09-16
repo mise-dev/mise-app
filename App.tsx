5b416e511c56efd9a98de107fae6aac6a18d4bfe
JosiasAurel <ndjosiasaurel@gmail.com>
2023-09-16 18:04:11 +0100

    proto custom header

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
-import { GluestackUIProvider,config } from "@gluestack-ui/themed"
+import { GluestackUIProvider, config } from "@gluestack-ui/themed"
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HStack, Text } from "@gluestack-ui/themed";
+import { ProfileCircle, Cart } from "iconoir-react-native";
 
 // import screens here
 import Home from "./Views/Home";
 import KitchenSink from "./Views/KitchenSink";
 
 const Stack = createNativeStackNavigator();
+
+// configuring the header
+const headerOptions = {
+  headerLeft: ({ children }) => <Text bold color="black">{children}</Text>,
+  headerRight: () => (
+    <>
+      <HStack space="4xl">
+        <ProfileCircle color={"black"} width={30} height={30} />
+        <Cart color={"black"} width={30} height={30} />
+      </HStack>
+    </>
+  )
+};
 
 export default function App() {
   return (
@@ -17,6 +32,7 @@ export default function App() {
           <Stack.Screen
             name="Home"
             component={Home}
+            options={headerOptions}
           />
           <Stack.Screen name="Kitchen" component={KitchenSink} />
 
