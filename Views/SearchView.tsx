d3783c58745ae2f4f070f1ed3947beab1f78604e
unknown <glenmue2020@gmail.com>
2023-10-25 17:29:36 +0100

    finished the search view and amde a discent design still have to make a few functions

--- Views/SearchView.tsx
+++ Views/SearchView.tsx
@@ -28,28 +28,66 @@ const SearchView = () => {
   const [value, setValue] = useState("");
 
   return (
-    <>
-      <Input
-        size={"lg"}
-        variant={"outline"}
-        isInvalid={false}
-        isDisabled={false}
-        w={"$full"}
-        p={"$2"}
-        borderRadius={"$full"}
-      >
-        <InputIcon pr="$4" h={"$4"} alignSelf="center">
-          <Icon as={SearchIcon} />
-        </InputIcon>
-        <InputField
-          onChange={(e: any) => {
-            setValue(e.nativeEvent.text);
-          }}
-          value={value}
-          placeholder="Enter Text here"
-        />
-      </Input>
-    </>
+    <Box bgColor="white" h={"$full"} p={"$4"}>
+      <Center marginBottom={"$2"}>
+        <HStack>
+          <Input
+            size={"lg"}
+            variant={"outline"}
+            isInvalid={false}
+            isDisabled={false}
+            w={"65%"}
+            p={"$2"}
+            borderRadius={"$full"}
+          >
+            <InputIcon pr="$4" h={"$4"} alignSelf="center">
+              <Icon as={SearchIcon} />
+            </InputIcon>
+            <InputField
+              onChange={(e: any) => {
+                setValue(e.nativeEvent.text);
+              }}
+              value={value}
+              placeholder="Enter Text here"
+            />
+          </Input>
+          <Button bgColor="transparent">
+            <ButtonText color="black">Search</ButtonText>
+          </Button>
+        </HStack>
+      </Center>
+      <Text marginTop={"$2"} color="grey">
+        Popular Searches
+      </Text>
+      <HStack marginTop={"$2"}>
+        <Button bgColor="#E5E4E2" borderRadius={"$full"} marginRight={"$2"}>
+          <ButtonText fontWeight="thin" color="grey">
+            barbie
+          </ButtonText>
+        </Button>
+        <Button bgColor="#E5E4E2" borderRadius={"$full"} marginRight={"$2"}>
+          <ButtonText fontWeight="thin" color="grey">
+            durag
+          </ButtonText>
+        </Button>
+      </HStack>
+
+      <Text marginTop={"$2"} color="grey">
+        Recent searches
+      </Text>
+      <HStack marginTop={"$2"}>
+        <Button bgColor="#E5E4E2" borderRadius={"$full"} marginRight={"$2"}>
+          <ButtonText fontWeight="thin" color="grey">
+            compressor
+          </ButtonText>
+        </Button>
+        <Button bgColor="#E5E4E2" borderRadius={"$full"} marginRight={"$2"}>
+          <ButtonText fontWeight="thin" color="grey">
+            toyCar
+          </ButtonText>
+        </Button>
+      </HStack>
+    </Box>
   );
 };
 
--- navigation/Tabs.tsx
+++ navigation/Tabs.tsx
@@ -6,46 +6,107 @@ import ShopProfile from "../Views/Shop";
 import KitchenSink from "../Views/KitchenSink";
 import { Box } from "@gluestack-ui/themed";
 
-const renderTabBar = (props) => (
-  <TabBar
-    {...props}
-    indicatorStyle={{ backgroundColor: "#000" }}
-    style={{ backgroundColor: "#fff" }}
-    alignSelf="flex-end"
-  />
-);
+// const renderTabBar = (props) => (
+//   <TabBar
+//     {...props}
+//     indicatorStyle={{ backgroundColor: "#000" }}
+//     style={{ backgroundColor: "#fff" }}
+//     alignSelf="flex-end"
+//   />
+// );
 
-const TabApp = () => {
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    { key: "home", title: "Home" },
-    { key: "shop", title: "Shop" },
-    { key: "cart", title: "Cart" },
-  ]);
+// const TabApp = () => {
+//   const [index, setIndex] = React.useState(0);
+//   const [routes] = React.useState([
+//     { key: "home", title: "Home" },
+//     { key: "shop", title: "Shop" },
+//     { key: "cart", title: "Cart" },
+//   ]);
 
-  const renderScene = ({ route }) => {
-    switch (route.key) {
-      case "home":
-        return <Home />;
-      case "shop":
-        return <ShopProfile />;
-      case "cart":
-        return <KitchenSink />;
-      default:
-        return null;
-    }
-  };
+//   const renderScene = ({ route }) => {
+//     switch (route.key) {
+//       case "home":
+//         return <Home />;
+//       case "shop":
+//         return <ShopProfile />;
+//       case "cart":
+//         return <KitchenSink />;
+//       default:
+//         return null;
+//     }
+//   };
 
+//   return (
+//     <Box flex={1} flexDirection="column" justifyContent="space-between">
+//       <TabView
+//         navigationState={{ index, routes }}
+//         renderScene={renderScene}
+//         onIndexChange={setIndex}
+//         renderTabBar={renderTabBar}
+//       />
+//     </Box>
+//   );
+// };
+
+// export default TabApp;
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+
+function App() {
   return (
-    <Box flex={1} flexDirection="column" justifyContent="space-between">
-      <TabView
-        navigationState={{ index, routes }}
-        renderScene={renderScene}
-        onIndexChange={setIndex}
-        renderTabBar={renderTabBar}
-      />
-    </Box>
+    <NavigationContainer>
+      <Tab.Navigator tabBar={(props) => <MyTabBar {...props} />}>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={
+                  focused
+                    ? require("./home_active.png")
+                    : require("./home_inactive.png")
+                }
+                style={{ width: 20, height: 20 }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Shop"
+          component={ShopScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={
+                  focused
+                    ? require("./shop_active.png")
+                    : require("./shop_inactive.png")
+                }
+                style={{ width: 20, height: 20 }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Cartr"
+          component={CartrScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={
+                  focused
+                    ? require("./cart_active.png")
+                    : require("./cart_inactive.png")
+                }
+                style={{ width: 20, height: 20 }}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-};
-
-export default TabApp;
+}
